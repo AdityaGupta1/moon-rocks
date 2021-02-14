283a1607af7602a06de18a46748cfbd39233f5ab
AdityaGupta1 <adityag@gmail.com>
2021-02-13 21:14:22 -0500

    eraser tooltip

--- src/main/java/org/sdoaj/moonrocks/common/items/EraserItem.java
+++ src/main/java/org/sdoaj/moonrocks/common/items/EraserItem.java
@@ -1,15 +1,30 @@
 package org.sdoaj.moonrocks.common.items;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class EraserItem extends Item {
     public EraserItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
+        Style style = Style.EMPTY.createStyleFromFormattings(TextFormatting.DARK_PURPLE, TextFormatting.ITALIC);
+        tooltip.add(new StringTextComponent("There are many things").setStyle(style));
+        tooltip.add(new StringTextComponent("that need to be erased").setStyle(style));
     }
 
     @Override
